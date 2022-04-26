22e7e53b863b28f48669ad8943bc1c3ab46728d5
Kirill Malskiy <malskykirill@yandex.ru>
2022-04-26 12:26:09 +0300

    Начинаем программировать

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.export = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  devtool: 'sourse-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'publick' }],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: ['babel-loader']
+      }
+    ]
+  }
+};
+
+
