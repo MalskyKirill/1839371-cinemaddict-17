a093a19280a011e0681d411972865560ccb3a50c
Kirill Malskiy <malskykirill@yandex.ru>
2022-05-02 23:12:04 +0300

    Шаблонизируй это(вынес создание коментариев попап в отдельную функцию)

--- src/view/popup-view.js
+++ src/view/popup-view.js
@@ -2,6 +2,32 @@ import { createElement } from '../render.js';
 import { humanizeFilmReleaseDate } from '../util.js';
 import { getTimeFromMins } from '../util.js';
 
+const createCommentsTemplate = (comments) => {
+
+  let str = '';
+
+  for(let i = 0; i < comments.length; i++) {
+
+    const date = humanizeFilmReleaseDate(comments[i].date);
+    str += `
+    <li class="film-details__comment">
+      <span class="film-details__comment-emoji">
+        <img src="./images/emoji/${comments[i].emotion}.png" width="55" height="55" alt="emoji-sleeping">
+      </span>
+      <div>
+        <p class="film-details__comment-text">${comments[i].comment}</p>
+        <p class="film-details__comment-info">
+          <span class="film-details__comment-author">${comments[i].author}</span>
+          <span class="film-details__comment-day">${date}</span>
+          <button class="film-details__comment-delete">Delete</button>
+        </p>
+      </div>
+    </li>
+     `;
+  }
+  return str;
+};
+
 const createPopupTemplate = (film) => {
   const {filmInfo, comments, userDetails} = film;
   const date = humanizeFilmReleaseDate(filmInfo.release.date);
@@ -97,32 +123,7 @@ const createPopupTemplate = (film) => {
               <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${comments.length}</span></h3>
 
               <ul class="film-details__comments-list">
-                <li class="film-details__comment">
-                  <span class="film-details__comment-emoji">
-                    <img src="./images/emoji/smile.png" width="55" height="55" alt="emoji-smile">
-                  </span>
-                  <div>
-                    <p class="film-details__comment-text">Interesting setting and a good cast</p>
-                    <p class="film-details__comment-info">
-                      <span class="film-details__comment-author">Tim Macoveev</span>
-                      <span class="film-details__comment-day">2019/12/31 23:59</span>
-                      <button class="film-details__comment-delete">Delete</button>
-                    </p>
-                  </div>
-                </li>
-                <li class="film-details__comment">
-                  <span class="film-details__comment-emoji">
-                    <img src="./images/emoji/sleeping.png" width="55" height="55" alt="emoji-sleeping">
-                  </span>
-                  <div>
-                    <p class="film-details__comment-text">Booooooooooring</p>
-                    <p class="film-details__comment-info">
-                      <span class="film-details__comment-author">John Doe</span>
-                      <span class="film-details__comment-day">2 days ago</span>
-                      <button class="film-details__comment-delete">Delete</button>
-                    </p>
-                  </div>
-                </li>
+                ${createCommentsTemplate(comments)}
               </ul>
 
               <div class="film-details__new-comment">
