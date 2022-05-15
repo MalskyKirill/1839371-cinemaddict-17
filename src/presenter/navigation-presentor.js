36a24198778c1b5a095e9897b1cb7dc89729f87a
Kirill Malskiy <malskykirill@yandex.ru>
2022-05-15 10:45:31 +0300

    4.10. Шаблонизируй то(подключил фильтры в main через presentor)

--- src/presenter/navigation-presentor.js
+++ src/presenter/navigation-presentor.js
@@ -1,11 +1,8 @@
 import NavigationView from '../view/navigation-view.js';
-import MoviesModel from '../model/movies-model.js';
-import { generateFilter } from '../mock/filter-fish.js';
 
 import { render } from '../framework/render.js';
 
 const siteMainElement = document.querySelector('.main');
-const moviesModel = new MoviesModel();
 
 
 export default class NavigationPresentor {
@@ -13,49 +10,8 @@ export default class NavigationPresentor {
 
   init = (filters) => {
     this.#filters = filters;
-    filters = generateFilter(moviesModel.movies);
 
     const navigation = new NavigationView(this.#filters);
     render (navigation, siteMainElement);
   };
 }
-// _____________
-// import PopupView from '../view/popup-view.js';
-// import { render, remove } from '../framework/render.js';
-
-// const siteFooterElement = document.querySelector('.footer');
-// const body = document.querySelector('body');
-
-// class PopupPresentor {
-
-//   #movie = null;
-
-//   init = (movie) => {
-//     this.#movie = movie;
-
-//     const popup = new PopupView(this.#movie);
-
-//     const onEscKeyDown = (evt) => {
-//       if (evt.key === 'Escape' || evt.key === 'Esc') {
-//         evt.preventDefault();
-//         body.classList.remove('hide-overflow');
-//         remove(popup);
-//         document.removeEventListener('keydown', onEscKeyDown);
-//       }
-//     };
-
-//     render(popup, siteFooterElement, 'afterend');
-
-//     body.classList.add('hide-overflow');
-
-//     popup.setPopupCloseClickHandler(()=> {
-//       body.classList.remove('hide-overflow');
-//       remove(popup);
-//       document.removeEventListener('keydown', onEscKeyDown);
-//     });
-
-//     document.addEventListener('keydown', onEscKeyDown);
-
-//   };
-
-// }
--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,7 +1,7 @@
 import { FilterType } from '../const.js';
 
 const filter = {
-  [FilterType.ALL_MOVIES]: (films) => films.filter((film) => !film.isAll),
+  [FilterType.ALL_MOVIES]: (films) => (films.map((film) => film)),
   [FilterType.WATCHLIST]: (films) => films.filter((film) => film.userDetails.watchlist),
   [FilterType.HISTORY]: (films) => films.filter((film) => film.userDetails.alreadyWatched),
   [FilterType.FAVORITES]: (films) => films.filter((film) => film.userDetails.favorite),
