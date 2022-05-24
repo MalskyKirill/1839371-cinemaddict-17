bf091d134ce6371aec4243084e296699d6bd33c3
Kirill Malskiy <malskykirill@yandex.ru>
2022-05-24 11:18:21 +0300

    5.6. Большие перемены (часть 2) (поправил кнопки сортировки)

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -2,7 +2,7 @@ import AbstractView from '../framework/view/abstract-view.js';
 import { SortType } from '../const.js';
 
 const createSortTemplate = () => (`<ul class="sort">
-                                        <li><a href="#" class="sort__button" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+                                        <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
                                         <li><a href="#" class="sort__button" data-sort-type="${SortType.SORRT_BY_DATE}">Sort by date</a></li>
                                         <li><a href="#" class="sort__button" data-sort-type="${SortType.SORT_BY_RATING}">Sort by rating</a></li>
                                       </ul>`);
@@ -20,15 +20,17 @@ export default class SortView extends AbstractView{
   };
 
   #sortTypeChangeHandler = (evt) => {
+    evt.preventDefault();
+
     if (evt.target.tagName !== 'A') {
       return;
     }
 
-    this.element.querySelectorAll('li').forEach((el) => el.addEventListener('click', () => {
-      this.element.classList.toggle('sort__button--active');
-    }));
+    this.element.querySelectorAll('li > a').forEach((el) =>  {
+      el.classList.remove('sort__button--active');
+    });
 
-    evt.preventDefault();
+    evt.target.classList.add('sort__button--active');
 
     this._callback.sortTypeChange(evt.target.dataset.sortType);
   };
